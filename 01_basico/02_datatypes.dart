c7b5aeae6fe77b800b251bcd1e380d70160ef127
Juan Guillermo Hernandez Alarcon <juanguillermoalarcon@gmail.com>
2021-02-27 00:13:00 -0500

    12. Tipado de datos: Strings

--- 01_basico/02_datatypes.dart
+++ 01_basico/02_datatypes.dart
@@ -3,5 +3,24 @@ main() {
   var a = 10; //tipado dinamico
 
   int _a = 30; //importante el guion bajo para clases.
-  print(_a);
+  //print(_a);
+
+  //strings
+
+  String nombre = 'Hola';
+  String nombre2 = "Ola";
+
+  String nombre3 = "O'Conor";
+  print(nombre3);
+  String nombre4 =
+      'O\'Conor'; //se recomiendan las comillas simples por ser menos pesadas
+  print(nombre4);
+
+  String muiltilinea = '''
+  Hola
+  mundo
+  cruel que
+    tal''';
+
+  print(muiltilinea);
 }
